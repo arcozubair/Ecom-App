77d1f2a78071cbc26a8d01855d4b1d4c699d44b1
arcozubair <arcozubair@gmail.com>
2026-08-17 12:14:01 +0530

    feat: initialize mobile project configuration and configure axios API client with authentication interceptors

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -3,8 +3,8 @@ import { Platform } from 'react-native';
 import { useAuthStore } from '../store/useAuthStore';
 
 
-// const API_URL = 'https://ecom-app-s20s.onrender.com/api'; //production
-const API_URL = 'http://192.168.17.222:3000/api';
+const API_URL = 'https://ecom-app-s20s.onrender.com/api'; //production
+// const API_URL = 'http://192.168.17.222:3000/api';
 
 
 const apiClient = axios.create({
